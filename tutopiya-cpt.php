baaa7de99b873b366511b3601a459add28bc95f7
Minhaz Irphan Mohamed <MinhazI@users.noreply.github.com>
2024-06-03 21:21:51 +0530

    Added meta tags and the schema

--- tutopiya-cpt.php
+++ tutopiya-cpt.php
@@ -156,3 +156,43 @@ function tutopiya_template_redirect($template)
 }
 
 add_filter('template_include', 'tutopiya_template_redirect', 99);
+
+function add_schema_markup()
+{
+    if (is_singular('educational_article')) {
+        global $post;
+        $schema = array(
+            '@context' => "http://schema.org",
+            '@type' => "Article",
+            'mainEntityOfPage' => get_permalink($post->ID),
+            'headline' => get_the_title($post->ID),
+            'datePublished' => get_post_meta($post->ID, 'publication_date', true),
+            'author' => array(
+                '@type' => 'Person',
+                'name' => get_post_meta($post->ID, 'author_name', true)
+            ),
+            'image' => get_the_post_thumbnail_url($post->ID, 'full')
+        );
+        echo '<script type="application/ld+json">' . wp_json_encode($schema, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . '</script>' . "\n";
+    }
+}
+add_action('wp_head', 'add_schema_markup');
+
+function add_seo_meta_tags()
+{
+    if (is_singular('educational_article')) {
+        global $post;
+        $excerpt = get_the_excerpt($post->ID);
+        $title = get_the_title($post->ID);
+        $url = get_permalink($post->ID);
+        $thumbnail = get_the_post_thumbnail_url($post->ID, 'full');
+
+        echo '<meta name="description" content="' . esc_attr($excerpt) . '" />' . "\n";
+        echo '<meta property="og:title" content="' . esc_attr($title) . '" />' . "\n";
+        echo '<meta property="og:description" content="' . esc_attr($excerpt) . '" />' . "\n";
+        echo '<meta property="og:type" content="article" />' . "\n";
+        echo '<meta property="og:url" content="' . esc_url($url) . '" />' . "\n";
+        echo '<meta property="og:image" content="' . esc_url($thumbnail) . '" />' . "\n";
+    }
+}
+add_action('wp_head', 'add_seo_meta_tags');
