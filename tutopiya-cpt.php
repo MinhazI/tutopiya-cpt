7b021be03c112478e62dab5df1d02161f9ba94a0
Minhaz Irphan Mohamed <MinhazI@users.noreply.github.com>
2024-06-04 09:45:31 +0530

    Added the code to allow users to add the subject from the "add article" page itself

--- tutopiya-cpt.php
+++ tutopiya-cpt.php
@@ -147,16 +147,84 @@ function tutopiya_display_publication_date_meta_box($post)
 function tutopiya_display_subject_category_meta_box($post)
 {
     wp_nonce_field(basename(__FILE__), 'tutopiya_nonce');
-    $subject_category = get_post_meta($post->ID, 'subject_category', true);
+    $subject_categories = get_post_meta($post->ID, 'subject_category', true) ?: array();
     $categories = get_terms(array(
         'taxonomy' => 'subject_category',
         'hide_empty' => false,
     ));
-    echo '<select name="subject_category" class="widefat">';
-    foreach ($categories as $category) {
-        echo '<option value="' . esc_attr($category->name) . '"' . selected($subject_category, $category->name, false) . '>' . esc_html($category->name) . '</option>';
-    }
-    echo "</select>";
+
+?>
+    <ul class="categorychecklist form-no-clear">
+        <?php foreach ($categories as $category) : ?>
+            <li>
+                <label class="selectit">
+                    <input type="checkbox" name="subject_category[]" value="<?php echo esc_attr($category->term_id); ?>" <?php checked(in_array($category->term_id, $subject_categories)); ?>>
+                    <?php echo esc_html($category->name); ?>
+                </label>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+    <p class="add-new-subject">
+        <a href="#" class="link"><?php _e('Add New Subject'); ?></a>
+    </p>
+    <div class="new-subject-form" style="display:none;">
+        <p>
+            <label for="new-subject-title"><?php _e('Subject Title:'); ?></label><br>
+            <input type="text" id="new-subject-title" name="new_subject_title">
+        </p>
+        <p>
+            <label for="new-subject-parent"><?php _e('Parent Subject:'); ?></label><br>
+            <select id="new-subject-parent" name="new_subject_parent">
+                <option value=""><?php _e('Parent Subject'); ?></option>
+                <?php foreach ($categories as $category) : ?>
+                    <option value="<?php echo esc_attr($category->term_id); ?>"><?php echo esc_html($category->name); ?></option>
+                <?php endforeach; ?>
+            </select>
+        </p>
+        <p>
+            <button id="add-new-subject" class="button-primary"><?php _e('Add New Subject'); ?></button>
+        </p>
+    </div>
+
+    <script>
+        jQuery(document).ready(function($) {
+            $('.add-new-subject a').click(function(e) {
+                e.preventDefault();
+                $('.new-subject-form').slideToggle();
+            });
+
+            $('#add-new-subject').click(function(e) {
+                e.preventDefault();
+
+                var title = $('#new-subject-title').val();
+                var parent = $('#new-subject-parent').val();
+
+                if (title === '') {
+                    alert('<?php _e('Please enter a subject title.'); ?>');
+                    return;
+                }
+
+                $.ajax({
+                    method: 'POST',
+                    url: ajaxurl,
+                    data: {
+                        action: 'tutopiya_add_new_subject',
+                        title: title,
+                        parent: parent,
+                        nonce: '<?php echo wp_create_nonce('tutopiya_nonce'); ?>'
+                    },
+                    success: function(response) {
+                        if (response.success) {
+                            location.reload();
+                        } else {
+                            alert(response.data);
+                        }
+                    }
+                });
+            });
+        });
+    </script>
+<?php
 }
 
 function tutopiya_save_meta_boxes($post_id)
@@ -171,11 +239,37 @@ function tutopiya_save_meta_boxes($post_id)
         update_post_meta($post_id, 'publication_date', sanitize_text_field($_POST['publication_date']));
     }
     if (isset($_POST['subject_category'])) {
-        update_post_meta($post_id, 'subject_category', sanitize_text_field($_POST['subject_category']));
+        update_post_meta($post_id, 'subject_category', array_map('sanitize_text_field', $_POST['subject_category']));
     }
 }
 
 add_action('save_post', 'tutopiya_save_meta_boxes');
+
+function tutopiya_add_new_subject_callback()
+{
+    check_ajax_referer('tutopiya_nonce', 'nonce');
+
+    if (!current_user_can('manage_categories')) {
+        wp_send_json_error(__('You do not have permission to add categories.'));
+    }
+
+    $title = sanitize_text_field($_POST['title']);
+    $parent = isset($_POST['parent']) ? (int)$_POST['parent'] : 0;
+
+    if (term_exists($title, 'subject_category')) {
+        wp_send_json_error(__('Subject category already exists.'));
+    }
+
+    $result = wp_insert_term($title, 'subject_category', array('parent' => $parent));
+
+    if (is_wp_error($result)) {
+        wp_send_json_error($result->get_error_message());
+    }
+
+    wp_send_json_success();
+}
+
+add_action('wp_ajax_tutopiya_add_new_subject', 'tutopiya_add_new_subject_callback');
 
 function tutopiya_template_redirect($template)
 {
